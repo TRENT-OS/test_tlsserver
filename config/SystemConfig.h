f13df367ecdc0fc28f489a3ae570015ca8ab83cf
Thomas Böhm <thomas.boehm@hensoldt-cyber.de>
2021-03-23 16:55:37 +0100

    [DEV] SEOS-2444 Allow Debug_Config_LOG_LEVEL to be set by cmake

--- config/SystemConfig.h
+++ config/SystemConfig.h
@@ -19,7 +19,9 @@
 #   define Debug_Config_NO_ASSERT_SELF_PTR
 #endif
 
+#if !defined(Debug_Config_LOG_LEVEL)
 #define Debug_Config_LOG_LEVEL              Debug_LOG_LEVEL_INFO
+#endif
 #define Debug_Config_INCLUDE_LEVEL_IN_MSG
 #define Debug_Config_LOG_WITH_FILE_LINE
 
