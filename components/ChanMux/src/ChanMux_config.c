b469999451c7a630476a8e17e552919799f33c9f
Felix Schladt <felix.schladt@hensoldt.net>
2024-02-23 10:55:31 +0100

    [DOC] Update Copyright
    Change files to GPLv2 or Commercial licensing

--- components/ChanMux/src/ChanMux_config.c
+++ components/ChanMux/src/ChanMux_config.c
@@ -1,7 +1,11 @@
 /*
  * ChanMux
  *
- * Copyright (C) 2020-2021, HENSOLDT Cyber GmbH
+ * Copyright (C) 2020-2024, HENSOLDT Cyber GmbH
+ * 
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ * For commercial licensing, contact: info.cyber@hensoldt.net
  */
 
 #include "ChanMux/ChanMux.h"
--- components/Tests/src/test_TlsServer.c
+++ components/Tests/src/test_TlsServer.c
@@ -1,7 +1,11 @@
 /*
  * Test System for TlsServer
  *
- * Copyright (C) 2019-2021, HENSOLDT Cyber GmbH
+ * Copyright (C) 2019-2024, HENSOLDT Cyber GmbH
+ * 
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ * For commercial licensing, contact: info.cyber@hensoldt.net
  */
 
 #include "lib_macros/Test.h"
